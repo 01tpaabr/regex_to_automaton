6ea8fb8ab61a105f248aba73d35bd1e2a2738315
01tpaabr <01tpaa.br@gmail.com>
2023-05-18 23:38:15 -0300

    run function

--- minicAutomaton.py
+++ minicAutomaton.py
@@ -1,6 +1,7 @@
 import automaton_generation
 import definitions
 import simplifyRegex
+import run
 
 regexTree = definitions.regexTree
 
@@ -9,7 +10,8 @@ regexTree = definitions.regexTree
 # '+', '!=', '>=', '=', 'int', 'number', '}', '>', ';', 'while', 'else', '/', '==']
 
 tokenAutomatonList = []
-priorityAutomatonList = []
+priorityList = []
+tokenType = []
 
 simpleTokenList = [
     '<=', 'if', '<', '{', '-', 'for', ',', '+', '!=', '>=', '=', 'int', '}', '>', ';', 'while', 'else', '/', '==', '\*', '\(', '\)'
@@ -24,11 +26,13 @@ for i in simpleTokenList:
 
     a = automaton_generation.genFinalAutomaton(newRegexTree)
     automaton_generation.removeEmpty(a)
-    if i != ",":
-        a.showVisualDFA("./token" + str(count) + ".png")
+    if i != ",": # usar virgula da problema com a biblioteca para printar
+        a.showVisualDFA("./automaton_images/token" + str(count) + ".png")
     
     tokenAutomatonList.append(a)
-    priorityAutomatonList.append(a)
+    if i[0] == "\\": i = i[1:]
+    tokenType.append(i)
+    priorityList.append(i)
 
     count += 1
 
@@ -43,14 +47,15 @@ otherTokenList = []
 
 number = simplifyRegex.simplify("([0-9]([0-9]*)( ))")
 otherTokenList.append(number)
+tokenType.append("number")
 
 identifier = simplifyRegex.simplify("(([a-z]|[A-Z])(([a-z]|[A-Z]|[0-9])*))")
 otherTokenList.append(identifier)
+tokenType.append("identifier")
 
 float = simplifyRegex.simplify("([0-9]([0-9]*)(.)([0-9]*)( ))")
 otherTokenList.append(float)
-
-
+tokenType.append("float")
 
 
 for i in otherTokenList:
@@ -60,10 +65,20 @@ for i in otherTokenList:
 
     a = automaton_generation.genFinalAutomaton(newRegexTree)
     automaton_generation.removeEmpty(a)
-    a.showVisualDFA("./token" + str(count) + ".png")
+    a.showVisualDFA("./automaton_images/token" + str(count) + ".png")
     
     tokenAutomatonList.append(a)
 
     count += 1
+
+
+
+inputFile = './ex_minic'
+
+with open(inputFile, 'r') as txt:
+    input = txt.read()
+
+result = run.run(tokenAutomatonList, priorityList, tokenType, input)
+print(result)
 
 
--- run.py
+++ run.py
@@ -1,26 +1,101 @@
 import automaton_generation
 
 #If input is valid, will return token list
-def run(automaton, input):
-    i = 0
+def run(tokenAutomatons, priorityList, tokenTypes, input):
+    notErrors = [" ", "\n"]
 
-    currState = automaton.initialState
+    #["token", "type", "line"]
     tokenList = []
 
+    inputPositions = []
+    accepting_automatons = []
+    possibleTokens = []
+    breakCharList = []
+    finishedList = []
+
+    for i in tokenAutomatons:
+        inputPositions.append(i.initialState)
+        possibleTokens.append("")
+        breakCharList.append("")
+        finishedList.append(False)
+
+    i = 0
     while i < len(input):
-        currChar = input[i]
+        
+        #When all runs has finished we will have a inputPositions list of the form [False, False, False, ...]
+        while False in finishedList:
+            currChar = input[i]
 
-        if currChar in currState.transitions:
-            currState = currState.transitions[currChar].target
-            currToken += currChar
+            print(possibleTokens)
+            print(currChar)
+            print()
+            
+            #One iteration for all automatons
+            j = 0
+            while j < len(inputPositions):
+                
+                currState = inputPositions[j]
+                if currState:
+                    if currChar not in currState.transitions:
+                        inputPositions[j] = False
+                        finishedList[j] = True
+
+                        if currState.final:
+                            accepting_automatons.append(tokenTypes[j])
+                            print(accepting_automatons)
+                        
+                        breakCharList[j] = currChar
+                    else:
+                        possibleTokens[j] += currChar
+                        inputPositions[j] = currState.transitions[currChar].target
+
+                j += 1
+
+            print(finishedList)
+            print(accepting_automatons)
+            print(input[i])
+            print()
+
+            #Get next char
+            if i + 1 <= len(input): i += 1
+            else: break
+        
+        #In the end of this while Loop, we will have an populated list of accepting_automatons
+        #Decide tokenType
+        if len(accepting_automatons) > 0:
+            tokenType = ""
+            for token in accepting_automatons:
+                if token in priorityList:
+                    tokenType = token
+                    break
+
+                tokenType = token
+            
+            tokenList.append([tokenType, possibleTokens[tokenTypes.index(tokenType)]])
         else:
-            #If there is no transitions, token ended
-            isValid = currState.final
+            #No automaton accepted this word
+            error = False
+            #If any automaton has last char different from " " or "\n" then they read something invalid
+            brokenChar = ""
 
-            if not isValid: return False
+            print(breakCharList)
+            for char in breakCharList:
+                if char not in notErrors:
+                    error = True
+                    brokenChar = char
+                    break
 
-            tokenList.append([currState.tokenType, currToken])
-            #Reset token
-            currToken = ""
-
-        i += 1
+            if error:
+                print("Token invalido: `" + str(brokenChar) + "`")
+                return False
+        
+        #Reset automatons
+        for a in range(len(tokenAutomatons)):
+            inputPositions[a] = tokenAutomatons[a].initialState
+            possibleTokens[a] = ""
+            breakCharList[a] = ""
+            finishedList[a] = False
+                
+        accepting_automatons = []
+    
+    return tokenList
